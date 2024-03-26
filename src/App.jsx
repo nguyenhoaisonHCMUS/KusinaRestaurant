5f89703cd674cddf39a0d23a791c1eeac287921d
son <son.9b.2015@gmail.com>
2024-03-26 20:39:24 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className=' text-3xl font-bold underline '>
+          hello
+        </div>      
+    </>
+  )
+}
+
+export default App
